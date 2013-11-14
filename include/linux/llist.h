7bd88ecdf893e3183a5f9bfe1057b62bbe831ed4
Christoph Hellwig <hch@infradead.org>
2013-11-14 14:32:11 -0800

    llists: move llist_reverse_order from raid5 to llist.c
    Make this useful helper available for other users.
    
    Signed-off-by: Christoph Hellwig <hch@lst.de>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Cc: Jens Axboe <axboe@kernel.dk>
    Cc: Neil Brown <neilb@suse.de>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
    Conflicts:
    	drivers/md/raid5.c
    
    Change-Id: Ibfc31e7289ffe9bda511c88543bc2deb70a4691b
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- include/linux/llist.h
+++ include/linux/llist.h
@@ -183,4 +183,6 @@ extern bool llist_add_batch(struct llist_node *new_first,
 			    struct llist_head *head);
 extern struct llist_node *llist_del_first(struct llist_head *head);
 
+struct llist_node *llist_reverse_order(struct llist_node *head);
+
 #endif /* LLIST_H */
