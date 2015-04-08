3c6c95477a418aa10b87f0f49a76b904e2187a70
Lynus Vaz <lvaz@codeaurora.org>
2015-04-08 22:56:16 +0530

    msm: kgsl: Notify the powerscale governor at state changes
    There were a few paths where the powerscale governor was not notified
    when the state was changed. Add the required calls so that the state
    as seen by the governor is consistent with the state of the device.
    
    CRs-Fixed: 808248
    Change-Id: I3167aa1642dd0627fd57c388cbf3d3b9f5444806
    Signed-off-by: Lynus Vaz <lvaz@codeaurora.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/gpu/msm/kgsl_pwrctrl.c
+++ drivers/gpu/msm/kgsl_pwrctrl.c
@@ -1851,6 +1851,7 @@ static int _init(struct kgsl_device *device)
 		kgsl_pwrctrl_irq(device, KGSL_PWRFLAGS_OFF);
 		del_timer_sync(&device->idle_timer);
 		device->ftbl->stop(device);
+		kgsl_pwrscale_sleep(device);
 		/* fall through */
 	case KGSL_STATE_AWARE:
 		kgsl_pwrctrl_disable(device);
@@ -1917,6 +1918,7 @@ static int _wake(struct kgsl_device *device)
 		/* Enable state before turning on irq */
 		kgsl_pwrctrl_set_state(device, KGSL_STATE_ACTIVE);
 		kgsl_pwrctrl_irq(device, KGSL_PWRFLAGS_ON);
+		kgsl_pwrscale_wake(device);
 		mod_timer(&device->idle_timer, jiffies +
 				device->pwrctrl.interval_timeout);
 		break;
@@ -1953,6 +1955,7 @@ _aware(struct kgsl_device *device)
 	case KGSL_STATE_SLEEP:
 		status = _wake(device);
 	case KGSL_STATE_ACTIVE:
+		kgsl_pwrscale_sleep(device);
 		kgsl_pwrctrl_irq(device, KGSL_PWRFLAGS_OFF);
 		del_timer_sync(&device->idle_timer);
 		break;
