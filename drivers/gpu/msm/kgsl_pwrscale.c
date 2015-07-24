257b2fe42b2ecaaf378eda399e45e7c193f53d10
Sunil Khatri <sunilkh@codeaurora.org>
2015-07-24 15:58:23 +0530

    msm: kgsl: Enable GPU POPP feature based on target
    Disable POPP feature by default for all targets
    and enable for a specific target by adding the
    entry in its dtsi file.
    
    Conflicts:
    	Documentation/devicetree/bindings/gpu/adreno.txt
    
    Change-Id: I1c2ed1af9e94470ef734b75bb2af3e093517b35d
    Signed-off-by: Sunil Khatri <sunilkh@codeaurora.org>
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/gpu/msm/kgsl_pwrscale.c
+++ drivers/gpu/msm/kgsl_pwrscale.c
@@ -738,6 +738,7 @@ int kgsl_pwrscale_init(struct device *dev, const char *governor)
 {
 	struct kgsl_device *device;
 	struct kgsl_pwrscale *pwrscale;
+	struct kgsl_device_platform_data *pdata;
 	struct kgsl_pwrctrl *pwr;
 	struct devfreq *devfreq;
 	struct devfreq *bus_devfreq;
@@ -751,6 +752,7 @@ int kgsl_pwrscale_init(struct device *dev, const char *governor)
 	if (device == NULL)
 		return -ENODEV;
 
+	pdata = dev_get_platdata(&device->pdev->dev);
 	pwrscale = &device->pwrscale;
 	pwr = &device->pwrctrl;
 	gpu_profile = &pwrscale->gpu_profile;
@@ -840,6 +842,13 @@ int kgsl_pwrscale_init(struct device *dev, const char *governor)
 				sizeof(struct kgsl_pwr_event), GFP_KERNEL);
 		pwrscale->history[i].type = i;
 	}
+
+	/*
+	 * Enable POPP feature if target supports it, by default
+	 * it is disabled.
+	 */
+	if (pdata->popp_enable)
+		set_bit(POPP_ON, &pwrscale->popp_state);
 
 	return 0;
 }
