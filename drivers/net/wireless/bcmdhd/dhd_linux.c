027e25aea293ced67d57fb64269a710f6bc3c295
franciscofranco <franciscofranco.1990@gmail.com>
2015-11-29 02:19:09 +0000

    wifi: bcmdhd: reduce ctrl wakelock timeout by half
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/net/wireless/bcmdhd/dhd_linux.c
+++ drivers/net/wireless/bcmdhd/dhd_linux.c
@@ -8872,7 +8872,7 @@ int dhd_os_wake_lock_timeout(dhd_pub_t *pub)
 				msecs_to_jiffies(dhd->wakelock_rx_timeout_enable) >> 1);
 		if (dhd->wakelock_ctrl_timeout_enable)
 			wake_lock_timeout(&dhd->wl_ctrlwake,
-				msecs_to_jiffies(dhd->wakelock_ctrl_timeout_enable));
+				msecs_to_jiffies(dhd->wakelock_ctrl_timeout_enable) >> 1);
 #endif
 		dhd->wakelock_rx_timeout_enable = 0;
 		dhd->wakelock_ctrl_timeout_enable = 0;
