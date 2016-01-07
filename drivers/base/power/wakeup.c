9005478973629e8278998c14bf2622566970b458
franciscofranco <franciscofranco.1990@gmail.com>
2016-01-07 17:28:37 +0000

    wakeup: add toggle for bluesleep wakelock
    Bluetooth might stop working properly with this wakeup source disabled, but it might be useful for users affected by this apparent bug.
    Bug-tracker: https://code.google.com/p/android/issues/detail?id=198180&q=bluesleep&colspec=ID%20Status%20Priority%20Owner%20Summary%20Stars%20Reporter%20Opened
    
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -28,6 +28,8 @@ static bool enable_wlan_wake_ws = true;
 module_param(enable_wlan_wake_ws, bool, 0644);
 static bool enable_bluedroid_timer_ws = true;
 module_param(enable_bluedroid_timer_ws, bool, 0644);
+static bool enable_bluesleep_ws = true;
+module_param(enable_bluesleep_ws, bool, 0644);
 
 /*
  * If set, the suspend/hibernate code will abort transitions to a sleep state
@@ -432,6 +434,9 @@ static void wakeup_source_activate(struct wakeup_source *ws)
                 return;
 
 	if (!enable_bluedroid_timer_ws && !strcmp(ws->name, "bluedroid_timer"))
+		return;
+
+	if (!enable_bluesleep_ws && !strcmp(ws->name, "bluesleep"))
 		return;
 
 	/*
