18b2438ba0b6db4d143a92141179f3d58cbaf7e6
Jaegeuk Kim <jaegeuk@kernel.org>
2016-06-02 16:45:12 -0700

    f2fs: Sync to Upstream
    * Up to date as of commit:
      3fb1acec3f18c96b53fd2b9a0e1c610abd150565
    
    Change-Id: I186667944c057eeb58d5bf8c8bce54b11f201447
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -45,6 +45,8 @@ struct vfsmount;
 struct cred;
 struct swap_info_struct;
 struct seq_file;
+struct fscrypt_info;
+struct fscrypt_operations;
 struct workqueue_struct;
 
 extern void __init inode_init(void);
@@ -607,6 +609,11 @@ struct inode {
 #ifdef CONFIG_IMA
 	atomic_t		i_readcount; /* struct files open RO */
 #endif
+
+#if IS_ENABLED(CONFIG_FS_ENCRYPTION)
+	struct fscrypt_info	*i_crypt_info;
+#endif
+
 	void			*i_private; /* fs or device private pointer */
 };
 
@@ -1262,6 +1269,9 @@ struct super_block {
 	const struct xattr_handler **s_xattr;
 
 	struct list_head	s_inodes;	/* all inodes */
+
+	const struct fscrypt_operations	*s_cop;
+
 	struct hlist_bl_head	s_anon;		/* anonymous dentries for (nfs) exporting */
 #ifdef CONFIG_SMP
 	struct list_head __percpu *s_files;
