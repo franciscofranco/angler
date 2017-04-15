e2c214486cc0f27684750e41f8a1a2baa392aaad
Sultanxda <sultanxda@gmail.com>
2017-04-15 03:40:27 +0000

    msm: qpnp-haptic: Use a high-priority workqueue for haptics
    Haptic requests are time-sensitive; they should not be delayed. Dump
    our worker into a high-prio workqueue to make sure everything gets done
    on time when overall system load is high.
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/platform/msm/qpnp-haptic.c
+++ drivers/platform/msm/qpnp-haptic.c
@@ -303,6 +303,7 @@ struct qpnp_hap {
 	spinlock_t td_lock;
 	struct work_struct td_work;
 	struct completion completion;
+	struct workqueue_struct *wq;
 	enum qpnp_hap_mode play_mode;
 	enum qpnp_hap_auto_res_mode auto_res_mode;
 	enum qpnp_hap_high_z lra_high_z;
@@ -1779,7 +1780,7 @@ static void qpnp_hap_td_enable(struct timed_output_dev *dev, int value)
         hap->td_value = value;
         spin_unlock(&hap->td_lock);
 
-        schedule_work(&hap->td_work);
+	queue_work(hap->wq, &hap->td_work);
 }
 
 void vibrate(int strength)
@@ -2443,6 +2444,12 @@ static int qpnp_haptic_probe(struct spmi_device *spmi)
 	rc = qpnp_hap_config(hap);
 	if (rc) {
 		dev_err(&spmi->dev, "hap config failed\n");
+		return rc;
+	}
+
+	hap->wq = alloc_workqueue("qpnp_haptics", WQ_HIGHPRI, 0);
+	if (!hap->wq) {
+		dev_err(&spmi->dev, "Failed to allocate workqueue\n");
 		return rc;
 	}
 
